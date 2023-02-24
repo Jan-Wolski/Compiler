a63377da60d0d2f68bee808039b38baa7009a370
Jan Wolski <261731@student.pwr.edu.pl>
2023-02-24 18:48:37 +0100

    Deletes unneded files.

--- Version1/enums.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-enum Operators : int {NOP,ADD,SUB,MUL,DIV,REM};
-enum Comparisons : int {NO,EQ,GT,LT,GTE,LTE,NEQ};
-enum class Ctrl : int {IF,IFELSE,WHILE,UNTIL};
-enum class Inst : int { GET, PUT, LOAD, STORE, LOADI, STOREI, ADD, SUB, ADDI, SUBI, SET, HALF, JUMP, JPOS, JZERO, JUMPI, HALT, CALL, LABEL };
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in programs/*
-do
-	echo "Testing: $i"
-	./project/kompilator "$i"
-done
-
